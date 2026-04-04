7074291e9e79328cb177b0bbfcb996f1532eb4ab
Barzin-Vazifedoost <barzin@extremityvault.dev>
2026-04-03 21:43:07 -0400

    test commit

--- src/php/login.php
+++ src/php/login.php
@@ -5,11 +5,14 @@ require_once 'db.php';
 $data = json_decode(file_get_contents('php://input'), true);
 
 $email = $data['email'];
-
+$password = $data['password'];
 
 $email = filter_var($email, FILTER_SANITIZE_EMAIL);
 $validEmail = filter_var($email, FILTER_VALIDATE_EMAIL);
-
+if (!$validEmail) {
+    echo json_encode(['success' => false, 'error' => 'Invalid Email']);
+    exit();
+}
 
 
 ?>
